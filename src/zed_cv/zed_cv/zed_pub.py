5bea726d70a1a49f2588dcfa469f66087d9ae0df
jasper <49565505+wluxie@users.noreply.github.com>
2022-11-27 11:03:42 -0800

    oops

--- src/zed_cv/zed_cv/zed_pub.py
+++ src/zed_cv/zed_cv/zed_pub.py
@@ -1,54 +1,64 @@
-import rclpy                        # Python library for ROS 2
-from rclpy.node import Node         # Handles the creation of nodes
-from sensor_msgs.msg import Image   # Image is the message type
-from cv_bridge import CvBridge      # Package to convert between ROS and OpenCV Images
-import cv2                          # OpenCV library
- 
-class ZedSubscriber(Node):
-  def __init__(self):
-    super().__init__('zed_sub')
-    self.subscription = self.create_subscription(
-      Image, 
-      'video_frames', 
-      self.listener_callback, 
-      10)
-    self.subscription # prevent unused variable warning
-      
-    # Used to convert between ROS and OpenCV images
-    self.br = CvBridge()
-   
-  def listener_callback(self, data):
-    # Display the message on the console
-    self.get_logger().info('😩 Receiving video frame!! 😫')
- 
-    # Convert ROS Image message to OpenCV image
-    current_frame = self.br.imgmsg_to_cv2(data)
-    
-    # Display image
-    cv2.imshow("Zed view", current_frame)
-    
-    cv2.waitKey(1)
-  
+import rclpy                                    # Python Client Library for ROS 2
+from rclpy.node import Node                     # Handles the creation of nodes
+from sensor_msgs.msg import Image               # Image is the message type
+import cv2                                      # OpenCV library
+from cv_bridge import CvBridge                  # Package to convert between ROS and OpenCV Images
+import numpy as np                              # Create the node and spin it
+
+
+
+class ZedPublisher(Node):
+    def __init__(self):
+        super().__init__('zed_pub')
+        self.publisher = self.create_publisher(Image, 'video_frames', 10)
+        timer_period = 1/30                                                             # FPS = 1 / timer_period
+
+        self.timer = self.create_timer(timer_period, self.timer_callback)               # Create a timer with the callback function
+        self.cap = cv2.VideoCapture(0)                                                  # Initialize the camera
+        self.br = CvBridge()                                                            # Used to convert between ROS and OpenCV Images
+
+
+
+    def timer_callback(self):                                                       # Check if there are subscribers
+        success, frame = self.cap.read()                                            # Read a frame from the camera
+
+        if success:                                                                 # If there is a frame
+            # ==== Aruco detection =================================================
+            # ======================================================================
+            # frame = np.split(frame, 2, axis=1)[0]                                   #     Split the frame in two
+            # frame = cv2.resize(frame, (640, 360), interpolation=cv2.INTER_AREA)     #     Resize the frame
+
+            frame = frame[0:480, 0:640]
+
+            aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)            #     Create the aruco dictionary
+            parameters = cv2.aruco.DetectorParameters_create()                      #     Create the aruco parameters
+            corners, ids = cv2.aruco.detectMarkers(frame, aruco_dict, parameters=parameters)           #     Detect the markers
+            frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids)              #     Draw the markers
+            # ======================================================================
+            
+            self.get_logger().info('🤧 Publishing Zed frames 😮‍💨')               
+            self.publisher.publish(self.br.cv2_to_compressed_imgmsg(frame))         # Publish the frame
+
+        else:                                                                       # If there is no frame             
+            self.get_logger().info(f'😭 Unsuccessful frames capture 😭')  
+
+
+
 
 
 def main(args=None):
-  
-  # Initialize the rclpy library
-  rclpy.init(args=args)
-  
-  # Create the node
-  zed_sub = ZedSubscriber()
-  
-  # Spin the node so the callback function is called.
-  rclpy.spin(zed_sub)
-  
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
-  zed_sub.destroy_node()
-  
-  # Shutdown the ROS client library for Python
-  rclpy.shutdown()
-  
+
+    rclpy.init(args=args)                                          # Initialize the ROS client library for Python
+    zed_pub = ZedPublisher()                                       # Create the node
+
+    try:                                                           # Try to spin the node
+        rclpy.spin(zed_pub)                                        #     Spin the node
+    except Exception as e:                                         # Catch any error that occurs
+        zed_pub.get_logger().info(f'😭 Error: {e} 😭')            #     Print the error
+    finally:                                                       # Finally
+        zed_pub.cap.release()                                      #     Release the camera
+        zed_pub.destroy_node()                                     #     Destroy the node
+        rclpy.shutdown()                                           #     Shutdown the ROS client library for Python
+
 if __name__ == '__main__':
-  main()
+    main()
